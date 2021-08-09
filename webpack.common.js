0292440b226d265ffcbddd56b2e0c7a8bc338ff8
iiiGYGYiii <realGYGY@gmail.com>
2021-08-09 13:50:32 -0500

    Add webpack configuration

---
+++ webpack.common.js
@@ -0,0 +1,36 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: path.join(__dirname, 'src', 'index.js'),
+    output: {
+        path: path.join(__dirname, 'build'),
+    },
+    module:{
+        rules:[
+            {
+                test: /\.s?css$/i,
+                use:[
+                    'css-loader',
+                ],
+            },
+            {
+                test: /\.jsx?$/i,
+                use:{
+                    loader: 'babel-loader',
+                    options:{
+                        presets:[
+                            '@babel/preset-env',
+                            '@babel/preset-react',
+                        ],
+                    },
+                },
+            }
+        ]
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'src', 'index.html'),
+        }),
+    ],
+};
---
+++ webpack.dev.js
@@ -0,0 +1,28 @@
+const { mergeWithRules } = require('webpack-merge')
+const common = require('./webpack.common.js');
+
+module.exports = mergeWithRules({
+    module:{
+        rules:{
+            test: 'match',
+            use: 'prepend',
+        },
+    },
+})(common,{
+    mode: 'development',
+    devtool: 'inline-source-map',
+    devServer:{
+        contentBase: './build',
+        port: 3000,
+    },
+    module:{
+        rules:[
+            {
+                test: /\.s?css$/i,
+                use:[
+                    'style-loader',
+                ],
+            },
+        ]
+    },
+});
