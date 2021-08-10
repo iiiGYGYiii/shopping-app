1dd8cd2a8bb92a6c8bef0f9cbac90803b588a653
iiiGYGYiii <realGYGY@gmail.com>
2021-08-10 00:26:20 -0500

    Add basic functionality to the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { Route } from "wouter";
 
+import { Header } from "./components/components.js";
 import { HomePage, ShopPage } from "./pages/pages.js";
 
-const App = () =>{
-  return(<div className="App">
-    <Route path="/" component={HomePage} />
-    <Route path="/shop" component={ShopPage} />
-  </div>);
+const App = () => {
+  return (
+    <div className="App">
+      <Header />
+      <Route path="/" component={HomePage} />
+      <Route path="/shop" component={ShopPage} />
+    </div>
+  );
 };
 
 export default App;
