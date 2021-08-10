96b1171d663ac79dbab91693abdb3117c41099c6
iiiGYGYiii <realGYGY@gmail.com>
2021-08-09 23:44:56 -0500

    Add base files for the nav-bar of the website.

---
+++ src/components/Header/Header.component.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+import "./Header.styles.scss";
+
+const Header = () => <div></div>;
+
+export default Header;
--- src/components/components.js
+++ src/components/components.js
@@ -2,3 +2,4 @@ export { default as MenuItem } from "./MenuItem/MenuItem.component.jsx";
 export { default as Directory } from "./Directory/Directory.component.jsx";
 export { default as PreviewCollection } from "./PreviewCollection/PreviewCollection.component.jsx";
 export { default as CollectionItem } from "./CollectionItem/CollectionItem.component.jsx";
+export { default as Header } from "./Header/Header.component.jsx";
