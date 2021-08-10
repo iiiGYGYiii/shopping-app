06d8bc9a516a5706581b915da566105b7a89d6ce
iiiGYGYiii <realGYGY@gmail.com>
2021-08-09 19:52:49 -0500

    Fix folder structuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { HomePage } from "./components/components.js";
+import { HomePage } from "./pages/pages.js";
 
 const App = () =>{
   return(<div className="App">
