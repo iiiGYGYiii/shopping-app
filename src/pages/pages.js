1851bcd406be3d83765cf45807aa55f22f234cc5
iiiGYGYiii <realGYGY@gmail.com>
2021-08-11 18:13:26 -0500

    Refactor the Pages components to end with .page and not .component so it way more readable

--- src/pages/pages.js
+++ src/pages/pages.js
@@ -1,3 +1,3 @@
-export { default as HomePage } from "./HomePage/homepage.component";
-export { default as ShopPage } from "./Shop/Shop.component";
-export { default as LogSignUpPage } from "./LogSignUpPage/LogSignUpPage.component";
+export { default as HomePage } from "./HomePage/homepage.page";
+export { default as ShopPage } from "./Shop/Shop.page";
+export { default as LogSignUpPage } from "./LogSignUpPage/LogSignUpPage.page";
