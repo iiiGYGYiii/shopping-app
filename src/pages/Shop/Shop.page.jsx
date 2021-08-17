e1ebaeda69f2636e69c45a2c18750917bd6cac18
iiiGYGYiii <realGYGY@gmail.com>
2021-08-17 16:32:09 -0500

    Refactor the way Shop Page is built

--- src/pages/Shop/Shop.page.jsx
+++ src/pages/Shop/Shop.page.jsx
@@ -1,20 +1,11 @@
-import { useSelector } from "react-redux";
-import { createStructuredSelector } from "reselect";
-import { selectShopItems } from "../../redux/reducers/shop/shop.selectors";
-
 import "./Shop.styles.scss";
 
-import { PreviewCollection } from "../../components/components";
-const collectionSelector = createStructuredSelector({
-  collections: selectShopItems,
-});
+import { CollectionsOverview } from "../../components/components";
+
 const ShopPage = () => {
-  const { collections } = useSelector(collectionSelector);
   return (
     <div className="shop-page">
-      {collections.map(({ id, ...otherCollectionProps }) => (
-        <PreviewCollection key={id} {...otherCollectionProps} />
-      ))}
+      <CollectionsOverview />
     </div>
   );
 };
