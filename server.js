64d8cbd537c3b09464c62d9b8b0a1fb9c1058e41
iiiGYGYiii <realGYGY@gmail.com>
2021-09-01 23:20:53 -0500

    Add basic server with express

--- server.js
+++ server.js
@@ -1,3 +1,22 @@
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
+
+if (process.env.NODE_ENV !== "production") require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
+
+  app.route("*").get((req, res) => {
+    res.sendFile(path.join(__dirname, "client/build/index.html"));
+  });
+}
+
+app.listen(port, () => console.log(`Running on PORT: ${port}`));
